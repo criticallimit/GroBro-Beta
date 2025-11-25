c901743019d497568b325d620504685bf2611971
criticallimit <fish24@gmx.net>
2025-11-25 16:35:39 +0100

    Update client.py

--- grobro/ha/client.py
+++ grobro/ha/client.py
@@ -174,11 +174,9 @@ class Client:
                 # Identify batX_temp sensors
                 name = key  # key == "bat1_temp", "bat2_temp", etc.
                 if name.startswith("bat") and name.endswith("_temp"):
-                    if isinstance(value, (int, float)) and value == 0:
+                    if isinstance(value, (int, float)) and value == -273.1:
                         payload[key] = None
-                    else:
-                        payload[key] = value *10 - 273.1
-        # ENUM Mapping (must come AFTER our replacement!)
+                
         if known_registers:
             for key, value in list(payload.items()):
                 reg = known_registers.input_registers.get(key)
